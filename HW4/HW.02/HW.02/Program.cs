c73560ed48b5ff591770ac4a635fa2d7a1617854
Maria Levchenkova <marystaro1991@gmail.com>
2021-10-19 20:52:44 +0300

    Add homework 4

---
+++ HW4/HW.02/HW.02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                Console.WriteLine("enter mumber");
+                string s1 = Console.ReadLine();
+                int i1 = int.Parse(s1);
+                a[i] = i1;
+            }
+            Print(a);
+
+            Console.WriteLine("enter  new number ");
+            string s2 = Console.ReadLine();
+            int number = int.Parse(s2);
+
+            Console.WriteLine("enter position new number");
+            string s3 = Console.ReadLine();
+            int index = int.Parse(s3);
+
+            for (int i = a.Length - 1; i >= index + 1; i--)
+            {
+                a[i] = a[i - 1];
+            }
+            a[index] = number;
+            Print(a);
+        }
+
+        static void Print(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HW4/HW.03.stopwacht/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW._03.Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] a = new long [100_000_000];
+            Random random = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(0, 100000000);
+            }
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+
+            Array.Reverse(a);
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Restart();
+            Reverse(a);
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedMilliseconds);
+        }
+
+        static void Reverse(long[] a)
+        {
+            for (int i = 0; i <= a.Length / 2; i++)
+            {
+                long c = a[i];
+                a[i] = a[a.Length - 1 - i];
+                a[a.Length - 1 - i] = c;
+            }
+        }
+    }
+}
