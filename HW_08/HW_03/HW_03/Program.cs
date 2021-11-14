c6cd10034a461492954fc76bd8bf6a45c86b8f9c
Maria Levchenkova <marystaro1991@gmail.com>
2021-11-14 20:13:06 +0300

    Add task 3 for hw 08.

---
+++ HW_08/HW_03/HW_03/Program.cs
@@ -0,0 +1,49 @@
+﻿string addresses = "123 Main St.Louisville OH 43071,432 Main Long Roald St.Louisville OH 43071,768 High Streat Pollochsville NY 56432";
+
+Console.WriteLine(Travel(addresses, "OH 43071"));
+Console.WriteLine(Travel(addresses, "NY 56432"));
+
+// 123 Main St.Louisville OH 43071,432 Main Long Road St.Louisville OH 43071
+// OH 43071:Main St.Louisville,Main Long Road St.Louisville/123,432
+// OH 123:/
+static string Travel(string addresses, string zipcode)
+{
+    string streets = string.Empty;
+    string numbers = string.Empty;
+
+    string[] array = addresses.Split(',');
+
+    foreach (var address in array)
+    {
+        //123 Main St.Louisville OH 43071
+        string[] parts = address.Split(' ');
+        string code = parts[parts.Length - 2] + " " + parts[parts.Length - 1];
+
+        if (code == zipcode)
+        {
+            string number = parts[0];
+            if (numbers == string.Empty)
+            {
+                numbers = number;
+            }
+            else
+            {
+                numbers += "," + number;
+            }
+
+            string street = address.Replace(number, "").Replace(code, "");
+            street = street.Trim();
+
+            if (streets == string.Empty)
+            {
+                streets = street;
+            }
+            else
+            {
+                streets += "," + street;
+            }
+
+        }
+    }
+    return zipcode + ":" + streets + "/" + numbers;
+}
