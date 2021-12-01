c0f564012cbaad294ef8bbf33554ca8993b7ac21
Maria Levchenkova <marystaro1991@gmail.com>
2021-12-01 22:14:32 +0300

    Add hw 13

---
+++ HW13/Task1/Song.cs
@@ -0,0 +1,19 @@
+﻿using HW13;
+
+public class Song
+{
+    public Song(string title, int minutes, string author, int albumYear, Genre genre)
+    {
+        Title = title;
+        Minutes = minutes;
+        Author = author;
+        AlbumYear = albumYear;
+        Genre = genre;
+    }
+    public string Title { get; }
+    public int Minutes { get; }
+    public string Author { get; }
+    public int AlbumYear { get; }
+    public Genre Genre { get; }
+}
+
---
+++ HW13/Task1/SongService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW13
+{
+    public class SongService : ISongService
+    {
+        private readonly List<Song> songs;
+
+        public SongService()
+        {
+            songs = new List<Song>
+            {
+                new Song("Song1", 3, "Author1", 2021, Genre.ClassicalMusic),
+                new Song("Song2", 3, "Author2", 2021, Genre.ElectonicMusic),
+                new Song("Song3", 3, "Author3", 2021, Genre.Jazz),
+                new Song("Song4", 3, "Author4", 2021, Genre.HipHop),
+                new Song("Song5", 3, "Author5", 2021, Genre.Rock),
+            };
+        }
+        public List<Song> Search(Genre search)
+        {
+            List<Song> result = songs.Where(x => search.HasFlag(x.Genre)).ToList();
+            return result;
+        }
+    }
+}
